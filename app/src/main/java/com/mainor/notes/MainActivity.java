0d4c60ba5b87cab77d5ef56b64602254701d33e1
Dm <hello@ausalt.com>
2019-10-07 17:47:07 +0300

    4 commit - fragment

--- app/src/main/java/com/mainor/notes/MainActivity.java
+++ app/src/main/java/com/mainor/notes/MainActivity.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mainor.notes.dummy.DummyContent;
 
-public class MainActivity extends AppCompatActivity  {
+
+public class MainActivity extends AppCompatActivity implements NotesFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intentToNoteEdit = new Intent(this, NoteEditActivity.class);
-        startActivity(intentToNoteEdit);
+        getSupportFragmentManager().beginTransaction().add(R.id.mainactivity_notes_list, new NotesFragment()).commit();
+        //Intent intentToNoteEdit = new Intent(this, NoteEditActivity.class);
+        //startActivity(intentToNoteEdit);
     }
 
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
 }
